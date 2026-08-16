cd987afbefa3358d5181089bd830fbd67dc9285b
Nirob-Barman <niroborin111@gmail.com>
2026-08-16 11:03:52 +0600

    Polish user dashboard with page header and quick links

--- GoDrive.Client/src/pages/user/Dashboard.tsx
+++ GoDrive.Client/src/pages/user/Dashboard.tsx
@@ -1,25 +1,26 @@
 import { Link } from "react-router-dom";
 import { useAppSelector } from "../../redux/hooks";
 import { selectCurrentUser } from "../../redux/features/auth/authSlice";
+import PageHeader from "../../components/ui/PageHeader";
 
 export default function Dashboard() {
   const user = useAppSelector(selectCurrentUser);
 
   return (
     <div>
-      <h1>My Dashboard</h1>
-      <p>Welcome, {user?.fullName}.</p>
-      <ul>
-        <li>
-          <Link to="/reservations">My Reservations</Link>
-        </li>
-        <li>
-          <Link to="/profile">My Profile</Link>
-        </li>
-        <li>
-          <Link to="/cars">Browse Cars</Link>
-        </li>
-      </ul>
+      <PageHeader title="My Dashboard" subtitle={`Welcome, ${user?.fullName}.`} />
+
+      <div className="page-header-actions">
+        <Link to="/reservations" className="btn">
+          My Reservations
+        </Link>
+        <Link to="/profile" className="btn">
+          My Profile
+        </Link>
+        <Link to="/cars" className="btn btn-primary">
+          Browse Cars
+        </Link>
+      </div>
     </div>
   );
 }
